43fb3dd253887cdb6bcd8f9930cb89df916d3e4a
Monica Delgado <mnca.delgado@gmail.com>
2021-08-22 23:35:23 -1100

    adding a form

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React from 'react';
-
-let todoList = [
-  {
-    id: 1,
-    title: "Clone Repository",
-  },
-  {
-    id: 2,
-    title: "Write Lesson 1.1",
-  },
-  {
-    id: 3,
-    title: "Submit pull request",
-  },
-  {
-    id: 4,
-    title: "Slack the asssigmet",
-  },
-
-
-];
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
+    
 
 function App() {
   return (
     <div>
-      <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function (item) {
-          return (
-            <li key={item.id}>{item.title}</li>
-          );
-        })}
-      </ul>
+      <h1>To do List</h1>
+      <AddTodoForm />
+      <TodoList />
+
     </div>
   );
 }
